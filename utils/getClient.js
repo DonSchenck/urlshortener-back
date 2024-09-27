a7f79c6e9f6e74d347a2090b1da9fc56df9f2d64
Don Schenck <don.schenck@gmail.com>
2024-09-27 17:02:53 -0400

    References environment variables

--- utils/getClient.js
+++ utils/getClient.js
@@ -1,20 +1,16 @@
 const { Client } = require('pg');
-const DB = require("../utils/postgresConn").getDB();
-const DB_TABLE = require("../utils/postgresConn").getDBTable;
-const DB_PASSWORD = require("../utils/postgresConn").getDBPassword;
-const DB_SERVER = require("../utils/postgresConn").getDBServer;
-const DB_USER = require("../utils/postgresConn").getDBUser;
+require('dotenv').config();
 
 module.exports.getClient = async () => {
 
-    console.log('DB_USER:' + DB_USER);
-    console.log('DB_SERVER:' + DB_SERVER);
+    console.log('  DB_USER: ' + process.env.DB_USER);
+    console.log('DB_SERVER: ' + process.env.DB_SERVER);
 
     const client = new Client({
-        user: DB_USER,
-        host: DB_SERVER,
-        database: DB,
-        password: DB_PASSWORD,
+        user: process.env.DB_USER,
+        host: process.env.DB_SERVER,
+        database: "routes",
+        password: process.env.DB_PASSWORD,
         port: 5432, // default port for PostgreSQL
     });
     await client.connect();
