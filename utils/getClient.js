1cd60c4d90a412faea253e393b307b4740349add
Don Schenck <don.schenck@gmail.com>
2024-09-27 16:49:51 -0400

    logging

--- utils/getClient.js
+++ utils/getClient.js
@@ -6,6 +6,10 @@ const DB_SERVER = require("../utils/postgresConn").getDBServer;
 const DB_USER = require("../utils/postgresConn").getDBUser;
 
 module.exports.getClient = async () => {
+
+    console.log('DB_USER:' + DB_USER);
+    console.log('DB_SERVER:' + DB_SERVER);
+
     const client = new Client({
         user: DB_USER,
         host: DB_SERVER,
