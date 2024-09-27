947f1135d1f8656af0a93d0106aae42f5c6d38a8
Don Schenck <don.schenck@gmail.com>
2024-09-27 16:27:34 -0400

    fixes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # urlshortener-back
 Backend service for URL Shortener
+
+Environment variables:
+
+DB_USER
+DB_PASSWORD
+DB_SERVER i.e. OpenShift Service name
+
+Database is "urls".
+Table is "routes".
--- utils/read-all-routes.js
+++ utils/read-all-routes.js
@@ -3,7 +3,6 @@ const { getClient } = require("./getClient");
 module.exports.getAllRoutes = async() => {
   const client = await getClient();
   const allroutes = await client.query('SELECT route, url FROM routes ORDER BY route');
-  console.log(Object.keys(entries.rows?.[0]).join('\t'));
-  console.log(`${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`);
+  console.log("Getting all routes...");
   await client.end();
 };
