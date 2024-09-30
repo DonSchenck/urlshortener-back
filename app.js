a0cb041d2db17b3374b871bbf5f3b8e51e279323
Don Schenck <don.schenck@gmail.com>
2024-09-30 14:06:05 -0400

    Code to create table

--- app.js
+++ app.js
@@ -20,19 +20,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/urls", urlRouter);
 
-// BEGIN The following section of code will create the database table IF NECESSARY
-const { getClient } = require('./utils/getClient');
-
-// "route" is the shortened URL; "url" is the long (where it goes) URL
-(async () => {
-  console.log('table will be created now...');
-  const client = getClient();
-  let createTableQuery = 'CREATE TABLE IF NOT EXISTS routes (route STRING PRIMARY KEY NOT NULL, url varchar, date TIMESTAMP NOT NULL DEFAULT current_timestamp)';
-  const res = await client.query(createTableQuery);
-  console.log('Created table.');
-  await client.end();
-});
-// END
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,5 +36,19 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.send(err);
 });
+
+console.log('BEGIN BUILD TABLE');
+
+// BEGIN The following section of code will create the database table IF NECESSARY
+const { getClient } = require('./utils/getClient');
+const c = getClient();
+
+// "route" is the shortened URL; "url" is the long (where it goes) URL
+console.log('table will be created now...');
+const client = getClient();
+let createTableQuery = 'CREATE TABLE IF NOT EXISTS routes (route STRING PRIMARY KEY NOT NULL, url varchar, date TIMESTAMP NOT NULL DEFAULT current_timestamp)';
+const res = client.query(createTableQuery);
+console.log('Created table.');
+// END
 
 module.exports = app;
