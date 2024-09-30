247c9365b1e16da5543fd679c7815dbd2c01b5ef
Don Schenck <don.schenck@gmail.com>
2024-09-30 16:15:41 -0400

    Return rows

--- utils/read-all-routes.js
+++ utils/read-all-routes.js
@@ -5,5 +5,6 @@ module.exports.getAllRoutes = async() => {
   const allroutes = await client.query('SELECT route, url FROM routes ORDER BY route');
   console.log("Getting all routes...");
   console.log(allroutes);
+  return allroutes;
   await client.end();
 };
