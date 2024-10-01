6435aaf0465e3ec28ee7a5eef80632aa213af465
Don Schenck <don.schenck@gmail.com>
2024-10-01 15:53:35 -0400

    Fixed DB Client

--- utils/read-all-routes.js
+++ utils/read-all-routes.js
@@ -1,7 +1,7 @@
 const { Client } = require("./getClient");
 
 module.exports.getAllRoutes = async() => {
-  const client = await client();
+  const client = await Client();
   const allroutes = await client.query('SELECT route, url FROM routes ORDER BY route');
   console.log("Getting all routes...");
   console.log(allroutes);
