86866661eed368037c4327aa85b3480d482203b2
Don Schenck <don.schenck@gmail.com>
2024-10-02 12:52:06 -0400

    Added debugging logs

--- utils/upsert-route.js
+++ utils/upsert-route.js
@@ -2,7 +2,7 @@ const { Client } = require('./getClient');
 
 module.exports.upsertRoute = async(route, url) => {
   const client = await Client();
-  let upsertCmd = 'INSERT INTO routes (route, url) VALUES ($1,$2) ON CONFLICT (route) DO UPDATE SET url = $3, [`${route}`,`${url}`,`${url}`]';
+  let upsertCmd = `INSERT INTO routes (route, url) VALUES (${route},${url}) ON CONFLICT (route) DO UPDATE SET url = ${$url}`
 
   console.log("upsertCmd: " + upsertCmd);
 
