162ff15a042a9f6164fb58756ea4d82892d4cac0
Don Schenck <don.schenck@gmail.com>
2024-10-02 12:37:56 -0400

    Upsert fixed

--- utils/upsert-route.js
+++ utils/upsert-route.js
@@ -2,13 +2,9 @@ const { Client } = require('./getClient');
 
 module.exports.upsertRoute = async(route, url) => {
   const client = await Client();
-  var upsertCmd = `INSERT INTO routes (route, url)
-                   VALUES ($1,$2)
-                   ON CONFLICT (route)
-                   UPDATE SET url = $2,` [`${route}`,`${url}`];
+  let upsertCmd = `INSERT INTO routes (route, url) VALUES ($1,$2) ON CONFLICT (route) DO UPDATE SET url = $3,` [`${route}`,`${url}`,`${url}`];
 
-//  let insertRow = await client.query('INSERT INTO routes(route, url) VALUES($1,$2);', [`${route}`,`${url}`]);
-let insertRow = await client.query(upsertCmd);
-console.log(`Upserted ${insertRow.rowCount} row`);
+  let insertRow = await client.query(upsertCmd);
+  console.log(`Upserted ${insertRow.rowCount} row`);
   await client.end();
 };
